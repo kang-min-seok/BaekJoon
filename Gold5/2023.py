0e8804fa6e8e6844632f7264b06912d612d7347c
minseokKang <min1042004444@gmail.com>
2024-01-23 16:44:04 +0900

    2023번 정답🎉

--- Gold5/2023.py
+++ Gold5/2023.py
@@ -1,20 +1,35 @@
-def calculate_polygon_area(num_sides, coordinates):
-    area = 0.0
+def isPrimeNumber(Number):
+    if(Number % 2 == 0):
+        return False
+    else:
+        for i in range(3,Number//2,2):
+            if(Number % i == 0):
+                return False
+    return True
 
-    for i in range(num_sides):
-        j = (i + 1) % num_sides
-        area += coordinates[i][0] * coordinates[j][1]
-        area -= coordinates[j][0] * coordinates[i][1]
-    area = abs(area) / 2.0
+def isAmazingNumber(stack, digit, index):
+    result = []
+    if(digit == index):
+        for x in stack:
+            print(x)
+        return stack
+    else:
+        for i in stack:
+            for j in [1,3,7,9]:
+                checkNumber = str(i) + str(j)
+                if(isPrimeNumber(int(checkNumber))):
+                    result.append(int(checkNumber))
+
     
-    print(round(area,1))
-    return area
+    isAmazingNumber(result, digit, index+1)
 
-N = int(input()) 
-coordinates = []
 
-for i in range(N):
-    X, Y = map(int, input().split()) 
-    coordinates.append((X, Y)) 
 
-calculate_polygon_area(N, coordinates)
+N = int(input())
+
+basic = [2,3,5,7]
+if(N == 1):
+    for i in basic:
+        print(i)
+else:
+    isAmazingNumber(basic, N, 1)
